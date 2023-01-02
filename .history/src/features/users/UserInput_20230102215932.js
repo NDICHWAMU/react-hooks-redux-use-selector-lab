da5be7df8a7eae802d526b6b90769d2173044374
ndishaw <ndichumuriithi@gmail.com>
2023-01-02 19:04:38 +0000

    Completed

---
+++ .history/src/features/users/UserInput_20230102215932.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+
+function UserInput() {
+  const dispatch = useDispatch();
+
+  const [formData, setFormData] = useState({
+    username: "",
+    hometown: "",
+  });
+
+  function handleInputChange(event) {
+    setFormData({
+      ...formData,
+      [event.target.id]: event.target.value,
+    });
+  }
+
+  function handleOnSubmit(event) {
+    event.preventDefault();
+    dispatch({ type: "users/add", payload: formData });
+  }
+
+  const resetForm = () =>{
+    setFormData.username("")
+    
+  }
+  return (
+    <form onSubmit={handleOnSubmit}>
+      <p>
+        <input
+          type="text"
+          id="username"
+          onChange={handleInputChange}
+          placeholder="username"
+        />
+      </p>
+      <p>
+        <input
+          type="text"
+          id="hometown"
+          onChange={handleInputChange}
+          placeholder="hometown"
+        />
+      </p>
+      <input type="submit" />
+    </form>
+  );
+}
+
+export default UserInput;
